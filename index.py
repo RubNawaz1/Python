ec9c4df3429b137931d9f988e9523827b70e1efb
RubNawaz1 <rubnawaz.mahar@gmail.com>
2024-11-13 13:13:49 +0500

    uodate file

--- index.py
+++ index.py
@@ -1,31 +1,19 @@
-def myfunc(n):
-  return lambda a : a * n
-
-mytripler = myfunc(2)
-
-print(mytripler(14))
-
-x = lambda a, b : a - b
-print(x(5, 3))
-
-
-animals=[ "dog","goadssdsdt","Camel"]
-print(animals)
-animals.append("Monkey")
-
-print(animals)
-
-x = len("goadssdsdt")
-print(x)
-
-
-
-class Person:
+class Student:
   def __init__(self, name, age):
+    print("adding new Student")
     self.name = name
     self.age = age
 
-p1 = Person("John", 36)
+student1 = Student("Muhammed", 36)
 
-print(p1.name)
-print(p1.age)
+print(student1.name)
+print(student1.age)
+
+student2 = Student("Ali", 39)
+print(student2.name)
+print(student2.age)
+
+
+student3 = Student("Ahmed", 19)
+print(student3.name)
+print(student3.age)

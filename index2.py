a29253da1aeff5dcd2a6ceb9f121d027f09c3865
RubNawaz1 <rubnawaz.mahar@gmail.com>
2024-11-13 12:23:25 +0500

    add new file

---
+++ index2.py
@@ -0,0 +1,16 @@
+class Car:
+  brand1= "Honda"
+  color="red"
+  Model=2025
+  brand2= "Toyota"
+  color2="Green"
+  Model2=2024
+  
+
+Car1 = Car()
+Car2 = Car()
+Car3 = Car()
+Car4 = Car()
+print(Car1.brand2)
+print(Car2.color)
+print(Car3.Model2)
